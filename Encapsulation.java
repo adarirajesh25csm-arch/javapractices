0e3ca25f9383e967ffbad5a815bd504b759b2746
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-08-07 11:39:30 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,52 @@
+package javacore;
+
+class Sample{
+   int a;
+   float b;
+   char c;
+   String s;
+
+public int getA() {
+	return a;
+
+}
+public void setA(int a) {
+	this.a = a;
+	
+}
+public float getB() {
+	return b;
+	
+}
+public void setB(float b) {
+	this.b = b;
+	
+}
+public char getC() {
+	return c;
+	
+}
+public void setC(char c) {
+	this.c = c;
+}
+public String getS() {
+	return s;
+}
+public void setS(String s) {
+	this.s = s;
+	
+}
+}
+public class Encapsulation{
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.setA(5000);
+		System.out.println(s.getA());   
+		
+		s.setB(3.345f);
+		System.out.println(s.getB());
+		
+		
+	}
+	
+}  
