26b604e5e8fdcc4c60a39fc5a05fa5abd14b50fc
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-08-28 07:33:46 +0530

    Update Bank management.java

--- recordprograms/Week 1,2/Bank management.java
+++ recordprograms/Week 1,2/Bank management.java
@@ -16,7 +16,7 @@ class account{
     void withdraw(int amount){
         if (balance>=amount) {
             balance=balance-amount;
-            System.out.println("balance after deposit is :"+balance);
+            System.out.println("balance after withdraw is :"+balance);
 
         }
         else
