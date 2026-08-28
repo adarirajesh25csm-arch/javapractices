25d02148ce6d4682c5eb08f232cadfa5a3f846e7
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-08-28 12:16:36 +0530

    Add FinallyExample class demonstrating exception handling

---
+++ Practiceprograms/ExceptionHandling/FinallyExample.java
@@ -0,0 +1,24 @@
+package exceptionhandling;
+
+public class FinallyExample {
+	
+	    public static void main(String[] args) {
+
+	        try {
+	            int a = 10;
+	            int b = 0;
+	            int result = a / b;  // ArithmeticException
+	            System.out.println(result);
+	        }
+	        catch (ArithmeticException e) {
+	            System.out.println("Cannot divide by zero");
+	        }
+	        finally {
+	            System.out.println("Finally block is always executed");
+	        }
+
+	        System.out.println("Program continues...");
+	    }
+	
+
+}
