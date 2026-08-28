5e8464d8509eb367ffa68a9e99b1e1d3013150ab
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-08-28 12:15:08 +0530

    Add Trywithmultiple2.java for exception handling example
    Demonstrates exception handling for arithmetic and array index errors.

---
+++ Practiceprograms/ExceptionHandling/Trywithmultiple2.java
@@ -0,0 +1,35 @@
+package exceptionhandling;
+
+public class Trywithmultiple2 {
+	public static void main(String[] args) {
+		
+		int a = 100;
+		int b = 0;
+		
+		
+	try {	
+		int c = a/b;  // Arithmetic Exception
+		
+		System.out.println("Value of C: " +c );
+
+		
+	}catch(ArithmeticException ae) { 
+		
+		System.out.println("It can execute only if you give non ZERO numbers");
+	}
+	try {
+		
+		int g[] = {10,20,30};
+		  
+		System.out.println("Index of 2: " +g[5]); // ArrayIndexOutOfBounds Exception
+	}
+		
+	catch(ArrayIndexOutOfBoundsException aioob) {
+		
+		System.out.println("The Array Index Is Out Of Bounds");
+	}
+		System.out.println("Program Executes Sucessfully");
+		
+	}
+
+}
