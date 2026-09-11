9de678a37ea470889fcbaa616d33156810157401
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-09-11 10:37:00 +0530

    Create StringOperations class with main method
    Added a new Java class to demonstrate string creation.

---
+++ Practiceprograms/StringOperations/create.java
@@ -0,0 +1,16 @@
+package string.operations;
+
+public class Create {
+	
+	public static void main(String[] args) {
+		String name = "Hello Boss!";             // Creating a String
+		String s1 = new String("Hello BhAAi");   // Creating a String
+		
+		System.out.println(name);
+		System.out.println(s1);
+		
+		
+		
+	}
+
+}
