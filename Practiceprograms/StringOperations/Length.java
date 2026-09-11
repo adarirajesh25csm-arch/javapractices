765592f4de4e7f0d89e10951b86e97671546f35d
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-09-11 10:41:48 +0530

    Add Length class for string length operations
    Implement Length class to demonstrate string length.

---
+++ Practiceprograms/StringOperations/Length.java
@@ -0,0 +1,17 @@
+package string.operations;
+
+public class  Length {
+	
+	public static void main(String[] args) {
+		String name = "Hello Boss!";             // Creating a String
+		String s1 = new String("Hello BhAAi");   // Creating a String
+		
+		System.out.println(name);
+		System.out.println(s1);
+		
+        System.out.println(name.length());  // length of a string
+        System.out.println(s1.length());
+		
+	}
+
+}
