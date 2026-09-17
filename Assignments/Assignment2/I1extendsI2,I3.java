f54019a72d02d349bb56acc385950de23c69385d
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-09-17 21:32:18 +0530

    Create I1extendsI2,I3.java

---
+++ Assignments/Assignment2/I1extendsI2,I3.java
@@ -0,0 +1,40 @@
+// I1 extends both I2 and I3.
+// Java allows an interface to extend multiple interfaces.
+
+interface I2 {
+    void method2();
+}
+
+interface I3 {
+    void method3();
+}
+
+interface I1 extends I2, I3 {
+    void method1();
+}
+
+class C1 implements I1 {
+
+    public void method1() {
+        System.out.println("Method of I1");
+    }
+
+    public void method2() {
+        System.out.println("Method of I2");
+    }
+
+    public void method3() {
+        System.out.println("Method of I3");
+    }
+}
+
+public class Case8 {
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.method1();
+        obj.method2();
+        obj.method3();
+    }
+}
