0b8b7e1f74230a47a0304dce1810a81b0adbf51e
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-09-17 21:29:03 +0530

    Add C1 class implementing interfaces I1 and I2

---
+++ Assignments/Assignment2/C1ImplementsI1,I2.java
@@ -0,0 +1,31 @@
+// C1 implements multiple interfaces I1 and I2.
+// Java supports multiple inheritance through interfaces.
+
+interface I1 {
+    void method1();
+}
+
+interface I2 {
+    void method2();
+}
+
+class C1 implements I1, I2 {
+
+    public void method1() {
+        System.out.println("Method of I1");
+    }
+
+    public void method2() {
+        System.out.println("Method of I2");
+    }
+}
+
+public class Case4 {
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.method1();
+        obj.method2();
+    }
+}
