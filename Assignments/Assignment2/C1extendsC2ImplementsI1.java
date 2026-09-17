0bf2ce7cbcaa79abc711b90d5d4f96414515dcca
adarirajesh25csm-arch <adarirajesh.25.csm@anits.edu.in>
2026-09-17 21:30:32 +0530

    Add C1 class extending C2 and implementing I1

---
+++ Assignments/Assignment2/C1extendsC2ImplementsI1.java
@@ -0,0 +1,29 @@
+// C1 extends class C2 and implements interface I1.
+// A class can extend one class and implement one or more interfaces.
+
+class C2 {
+    void displayC2() {
+        System.out.println("Method of C2");
+    }
+}
+
+interface I1 {
+    void displayI1();
+}
+
+class C1 extends C2 implements I1 {
+
+    public void displayI1() {
+        System.out.println("Method of I1");
+    }
+}
+
+public class Case5 {
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.displayC2();
+        obj.displayI1();
+    }
+}
